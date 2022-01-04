ac647284765c7b1c153fe6eaa1f78eb8938c1dac
BrkCoder <baribarakinbal@gmail.com>
2022-01-04 10:36:14 +0200

    favicon + layout changes :art:

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import Head from 'next/head'
 import styles from './layout.module.scss'
-const name = 'Your Name'
-export const siteTitle = `Title`
+import { identity } from './identity'
+const name = identity?.name;
+export const siteTitle = `${name}`
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Head>
-                <link rel="icon" href="./favicon.png" />
-                <meta name="description" content="Title" />
+                <link rel="icon" type="image/png" href="/images/favicon.png" />
+                <meta name="description" content={name} />
                 <meta name="og:title" content={siteTitle} />
             </Head>
-            <div className={styles.header}>
-            </div>
             <main className={styles.content}>{children}</main>
         </>
     )
